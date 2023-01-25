c089dbedc9a9cb4b3d3e1de50c5c57bbe6d52dda
Дмитрий Забродский <dmitrijzabrodskij@MacBook-Air-Dmitrij.local>
2023-01-26 01:33:19 +0300

    В программу добавлен цикл для ввода переменных

--- Program.cs
+++ Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите размер массива: ");
+int count = Convert.ToInt32(Console.ReadLine());
+
+int size = 3;
+String[] mass = new String[count];
+
+Console.WriteLine("Введите объекты массива: ");
+for(int i = 0; i < count; i++)
+{
+    mass[i] = Console.ReadLine();
+}
+
+
+for(int i = 0; i < count; i++)
+{
+    Console.WriteLine(mass[i]);
+}
+
+
+

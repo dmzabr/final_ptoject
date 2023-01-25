8ff8c3e67c0ad4689d49f1591192a72ddbfcfb2b
Дмитрий Забродский <dmitrijzabrodskij@MacBook-Air-Dmitrij.local>
2023-01-26 02:16:48 +0300

    Создание программы завершено

--- Program.cs
+++ Program.cs
@@ -1,20 +1,41 @@
 ﻿Console.WriteLine("Введите размер массива: ");
 int count = Convert.ToInt32(Console.ReadLine());
 
-int size = 3;
 String[] mass = new String[count];
 
 Console.WriteLine("Введите объекты массива: ");
-for(int i = 0; i < count; i++)
+
+for (int i = 0; i < count; i++)
 {
     mass[i] = Console.ReadLine();
 }
 
+Console.Write("\nРезультирующий массив: ");
+size_String(mass);
 
-for(int i = 0; i < count; i++)
+Console.ReadLine();
+
+
+int size_String(string[] mass)
 {
-    Console.WriteLine(mass[i]);
-}
+    int j = 0;
+    String[] result = new String[count];
 
+    for (int i = 0; i < mass.Length; i++)
+    {
+        int size = System.Text.ASCIIEncoding.Unicode.GetByteCount(mass[i]) / 2;
+        if (size <= 3)
+        {
+            result[j] = mass[i];
+            j++;
+        }
+    }
+
+    for (int i = 0; i < result.Length; i++)
+    {
+        Console.Write(result[i] + " ");
+    }
+    return 0;
+}
 
 
